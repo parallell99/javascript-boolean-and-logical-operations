c502daa45f2bf94ab2fea7316c8352a782f676ca
phi <patiparn.phi@gmail.com>
2025-12-04 14:18:59 +0700

    fix: update boolean logic and variable declarations in exercises 1, 2, and 3

--- ex-1.js
+++ ex-1.js
@@ -3,6 +3,10 @@ let hasCriminalBlacklist;
 
 let isAllow;
 
-// Start coding here
+isOver18 = 18;
+hasCriminalBlacklist = false;
+
+isAllow = (isOver18 >= 18) && !hasCriminalBlacklist;
+
 
 console.log(isAllow);
