db97d6809dc6c3d356690f02383d9d5b22a067f4
CasperTheBeagle <146039265+CasperTheBeagle@users.noreply.github.com>
2025-12-04 16:59:22 +0700

    JS complete

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
 let isOver18;
 let hasCriminalBlacklist;
-
 let isAllow;
 
 // Start coding here
+isOver18 = true; // James
+hasCriminalBlacklist = false; // never Crime
+isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
